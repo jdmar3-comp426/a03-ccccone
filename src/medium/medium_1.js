e63cc92bfdfffd9514d83dbdf12f4f69d8ac348c
ccccone <fx00123@gmail.com>
2021-11-07 17:16:35 -0500

    update

--- src/medium/medium_1.js
+++ src/medium/medium_1.js
@@ -25,7 +25,7 @@ export function getSum(array) {
  * let array = [3,2,5,6,2,7,4,2,7,5];
  * console.log(getMedian(array)); // 4.5
  */
-export function getMedian() {
+export function getMedian(array) {
     array.sort(function(a, b) {
         return a - b;
       });
