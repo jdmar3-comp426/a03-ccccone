6fa6e7ad7cadd4d36a17fc13c9e017e24e4e6bbc
ccccone <fx00123@gmail.com>
2021-11-11 16:34:27 -0500

    update

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -167,6 +167,7 @@ export const filter = (arr, test) => {
             ret.fail.push(arr[i]);
         }
     }
+    return ret;
 };
 
 
@@ -176,7 +177,7 @@ export const filter = (arr, test) => {
  *   odd numbers. Use the "everyEven" function in this function.
  */
 export const allEvensAreOdd = (arr) => {
-
+    return everyEven(arr, x=>x%2==1);
 };
 
 
